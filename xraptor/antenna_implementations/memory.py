ed34169cd82d15cea5fe4f64ad753cf57891ba0a
Marco Sievers de Almeida <im.ximit@gmail.com>
2025-01-29 20:06:55 -0300

    feat: memory antena

--- xraptor/antenna_implementations/memory.py
+++ xraptor/antenna_implementations/memory.py
@@ -12,11 +12,16 @@ class MemoryAntenna(Antenna):
     _queues = {}
     _config = {}
 
+    def __init__(self):
+        self._running = True
+
     async def subscribe(self, antenna_id: str) -> AsyncIterator:
         if antenna_id not in self._queues:
             self._queues.update({antenna_id: Queue()})
         _q: Queue = self._queues[antenna_id]
         while True:
+            if not self._running:
+                break
             if _q.empty() is False:
                 yield await _q.get()
             await asyncio.sleep(0.05)
@@ -26,6 +31,9 @@ class MemoryAntenna(Antenna):
             self._queues.update({antenna_id: Queue()})
         _q: Queue = self._queues[antenna_id]
         await _q.put(message)
+
+    async def stop_listening(self):
+        self._running = False
 
     async def is_alive(self, antenna_id: str) -> bool:
         return antenna_id in self._queues
